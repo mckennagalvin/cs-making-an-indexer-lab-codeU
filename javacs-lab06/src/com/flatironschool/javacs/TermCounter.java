b8c7fb3a5b2eb919c3f42dabbf89e725a4789261
mckennagalvin <mckennagalvin@me.com>
2016-06-03 23:17:08 -0700

    Done.

--- javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
@@ -36,8 +36,11 @@ public class TermCounter {
 	 * @return
 	 */
 	public int size() {
-        // TODO: fill this in.
-		return -1;
+		int total = 0;
+        for (String key: keySet()) {
+        	total += get(key);
+		}
+		return total;
 	}
 
 	/**
